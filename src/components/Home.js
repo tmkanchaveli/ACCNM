255f7bfbcd8619e8f6b4cfc92a1e45987877d130
tmkanchaveli <129501400+tmkanchaveli@users.noreply.github.com>
2023-04-04 19:40:22 +0400

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,30 +3,40 @@ import "./Home.css";
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <div className="hero">
+    <div className="home">
+      <div className="hero-section">
         <h1>Welcome to our Apartment Management App</h1>
         <p>Streamline your living with our user-friendly platform</p>
         <button>Get Started</button>
       </div>
-      <div className="features">
-        <div className="feature">
-          <h2>Task Management</h2>
-          <p>
-            Stay organized with a platform designed for your communal living
-          </p>
-          <button>Learn More</button>
+
+      <div className="features-section">
+        <h2>Features</h2>
+        <div className="feature-cards">
+          <div className="feature-card">
+            <h3>Task Management</h3>
+            <p>
+              Stay organized with a platform designed for your communal living
+            </p>
+            <button>Learn More</button>
+          </div>
+          <div className="feature-card">
+            <h3>Bill Payments</h3>
+            <p>Conveniently pay your bills directly through the app</p>
+            <button>Learn More</button>
+          </div>
+          <div className="feature-card">
+            <h3>Community Building</h3>
+            <p>Connect with your neighbors and build a stronger community</p>
+            <button>Learn More</button>
+          </div>
         </div>
-        <div className="feature">
-          <h2>Bill Payments</h2>
-          <p>Conveniently pay your bills directly through the app</p>
-          <button>Learn More</button>
-        </div>
-        <div className="feature">
-          <h2>Community Building</h2>
-          <p>Connect with your neighbors and build a stronger community</p>
-          <button>Learn More</button>
-        </div>
+      </div>
+
+      <div className="cta-section">
+        <h2>Join Our Community</h2>
+        <p>Start managing your community better with our platform.</p>
+        <button>Get Started</button>
       </div>
     </div>
   );
